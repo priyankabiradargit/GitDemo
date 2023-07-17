a1f575acd8cecfc4cf7382b8c546b51d5fdb58a4
priyanka <priyankabiradar91@gmail.com>
2023-07-17 20:19:26 +0530

    first commit

---
+++ src/browsertestpackage/TestDemo.java
@@ -0,0 +1,26 @@
+package browsertestpackage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void titleCheck() throws MalformedURLException
+	{
+		MutableCapabilities caps =  new MutableCapabilities();
+		
+		WebDriver driver =  new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), caps);
+		
+		driver.get("https://www.google.com/");
+		Assert.assertTrue(driver.getTitle().matches("Google"));
+		
+	}
+
+}
